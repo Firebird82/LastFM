8116c12b9296b3064296adff8d72d56bc460c117
cbublies <c.bublies@live.se>
2014-11-10 12:01:00 +0100

    Tagit bort onödiga rader

--- LastFM/MainActivity.cs
+++ LastFM/MainActivity.cs
@@ -6,7 +6,6 @@ using Android.Views;
 using Android.Widget;
 using Android.OS;
 using Android.Text;
-using Android 
 using System.Collections.Generic;
 
 namespace LastFM
@@ -41,11 +40,8 @@ namespace LastFM
 
 		public void SearchResult (string query)
 		{
-			RestSharp restSharp = new RestSharp();
-
-			data =  restSharp.GetSearchResult(query);
-			var items = new string[] { "Vegetables", "Fruits", "Flower Buds", "Legumes", "Bulbs", "Tubers" };
-			EditText searchQuery = FindViewById<EditText> (Resource.Id.searchtext);
+			data =  RestSharpFunctions.GetSearchResult(query);
+			EditText searchQuery = FindViewById<EditText> (Resource.Id.searchtext);	
 			ListView lView = FindViewById<ListView> (Resource.Id.listView1);
 			lView.Adapter = new ArtistSceenAdapter (this, data);
 			lView.ItemClick += OnListItemClick;
@@ -53,13 +49,9 @@ namespace LastFM
 
 		public void OnListItemClick(object sender, AdapterView.ItemClickEventArgs e)
 		{
-			var listView = sender as ListView;
-			var t = data[e.Position];
-
-			var balle = t.Name;
-
+			var clickedArtist = data[e.Position];
 			var intent = new Intent (this, typeof(ArtistViewActivity));
-			intent.PutExtra ("artist", t.Name);
+			intent.PutExtra ("artist", clickedArtist.Name);
 			StartActivity (intent);
 		
 
