7a7e307d2aad45e4ada9a8c1f6668f45ba0439bc
cbublies <c.bublies@live.se>
2014-11-11 12:40:51 +0100

    Kategoriknappar på sökningen

--- LastFM/MainActivity.cs
+++ LastFM/MainActivity.cs
@@ -29,51 +29,68 @@ namespace LastFM
 			albumList = new List<Album> ();
 		
 			Button artistSearchButton = FindViewById<Button> (Resource.Id.btnArtistSearch);
+			Button btnGetAlbums = FindViewById<Button> (Resource.Id.btnAlbumResult);
+			Button btnGetArtists = FindViewById<Button> (Resource.Id.btnArtistResult);
+			ListView searchResultListview = FindViewById<ListView> (Resource.Id.lvArtistSearchResult);
+
 			EditText artistSearchQery = FindViewById<EditText> (Resource.Id.artistSearchtext);
 			//Button albumSearchButton = FindViewById<Button> (Resource.Id.btnAlbumSearch);
 			//EditText albumSearchQery = FindViewById<EditText> (Resource.Id.albumSearchtext);
 
 			artistSearchButton.Click += delegate {
-				ArtistSearchResult (artistSearchQery.Text);
+
+				artistList.Clear();
+				albumList.Clear();
+
+				ArtistSearchResult (artistSearchQery.Text, searchResultListview);
 			};
-
-		/*	albumSearchButton.Click += delegate {
-				AlbumSearchResult (albumSearchQery.Text);
-			};
-		*/
-		}
-
-		public void ArtistSearchResult (string query)
-		{
-
-			ListView artistListview = FindViewById<ListView> (Resource.Id.lvArtistSearchResult);
-			artistList =  RestSharpFunctions.GetArtistList(query);
-			var tenArtist = artistList.GetRange (0,10);
-			artistListview.Adapter = new ArtistSceenAdapter (this, tenArtist);
-			artistListview.ItemClick += artistItemClick;
-		}
-
-
-		public void AlbumSearchResult(string query){
 		
-			ListView albumListview = FindViewById<ListView> (Resource.Id.lvArtistSearchResult);
-			albumList= RestSharpFunctions.GetAlbumList(query);
-			var tenAlbums = albumList.GetRange (0,10);
-			albumListview.Adapter = new AlbumScreenAdapter (this, tenAlbums);
-			albumListview.ItemClick += albumItemClick;		
+			btnGetAlbums.Click += delegate {
+				AlbumSearchResult (artistSearchQery.Text, searchResultListview);
+			};
+
+			btnGetArtists.Click += delegate {
+				ArtistSearchResult (artistSearchQery.Text, searchResultListview);
+			};
 		}
 
-		public void artistItemClick(object sender, AdapterView.ItemClickEventArgs e)
+		public void ArtistSearchResult (string query, ListView searchResultListview)
 		{
-			var clickedArtist = artistList[e.Position];
+			if (artistList.Count == 0) {
+
+				artistList = RestSharpFunctions.GetArtistList (query);
+
+			}
+
+			var tenArtist = artistList.GetRange (0, 10);
+			searchResultListview.Adapter = new ArtistSceenAdapter (this, tenArtist);
+			searchResultListview.ItemClick += artistItemClick;
+		}
+			
+		public void AlbumSearchResult (string query, ListView searchResultListview)
+		{
+			if (albumList.Count == 0) {
+
+				albumList = RestSharpFunctions.GetAlbumList (query);
+
+			}
+
+			var tenAlbums = albumList.GetRange (0, 10);
+			searchResultListview.Adapter = new AlbumScreenAdapter (this, tenAlbums);
+			searchResultListview.ItemClick += albumItemClick;		
+		}
+
+		public void artistItemClick (object sender, AdapterView.ItemClickEventArgs e)
+		{
+			var clickedArtist = artistList [e.Position];
 			var intent = new Intent (this, typeof(ArtistViewActivity));
 			intent.PutExtra ("artist", clickedArtist.Name);
 			StartActivity (intent);
 		}
 
-		public void albumItemClick(object sender, AdapterView.ItemClickEventArgs e)
+		public void albumItemClick (object sender, AdapterView.ItemClickEventArgs e)
 		{
-			var clickedAlbum = artistList[e.Position];
+			var clickedAlbum = artistList [e.Position];
 			var intent = new Intent (this, typeof(AlbumViewActivity));
 			intent.PutExtra ("album", clickedAlbum.Name);
 			StartActivity (intent);
